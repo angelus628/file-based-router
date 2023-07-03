ab83714c27c9150d6f94cefe679e1323528df91b
Luis Angel Agudelo Pajoy <angelus628@homail.com>
2023-07-03 22:13:17 +0200

    feat: events with no styles

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
+import EventList from "@/components/events/EventList";
+import { getFeaturedEvents } from "@/dummy-data";
+
 const HomePage = () => {
+    const featuredEvents = getFeaturedEvents();
     return (
         <div>
-            <h1>Home page</h1>
+            <EventList items={featuredEvents} />
         </div>
     );
 };
